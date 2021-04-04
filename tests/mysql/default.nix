254cc0b80196595d3ab1f2e6c4e482d348b3cb14
Sander van der Burg <svanderburg@gmail.com>
2021-04-04 22:11:40 +0200

    Allow the MySQL test to pass on Docker

--- tests/mysql/default.nix
+++ tests/mysql/default.nix
@@ -11,7 +11,7 @@ testService {
 
   tests = {instanceName, instance, runtimeDir, forceDisableUserChange, ...}:
     # Make a special exception for the first instance running in privileged mode. It should be connectible with the default settings
-    if instanceName == "mysql" && !forceDisableUserChange then ''
+    if instanceName == "mysql" && !forceDisableUserChange && runtimeDir == "/var/run" then ''
       machine.succeed("echo 'show databases;' | mysql >&2")
     '' else ''
       machine.succeed(
